86b9463b4c5748ae393479ce8aa4831d977a60be
len.zhong <lhongzhong@gmail.com>
2012-10-11 20:12:41 +0800

    Check license before running the server, still need to read data from Internet.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,39 @@
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "NIEngine.h"
 #include "SocketServer.h"
+
+using namespace std;
+
+bool CheckLicense()
+{
+	printf("validating your license...\n");
+	string keyword;
+	ifstream licenseFile;
+	licenseFile.open("license.txt");
+	if (licenseFile.is_open())
+	{
+		getline(licenseFile,keyword);
+		licenseFile.close();
+	}
+	printf("keyword:%s\n", keyword.c_str());
+
+	// call URL
+	string url = "http://api.activedooh.com/v1/" + keyword + "/status.txt";
+	printf(url.c_str());
+
+	// TODO: find the easiest way to read data from internet(url).
+
+	return true;
+}
 
 int main(int argc, char** argv)
 {
 	printf("NIServer Running...\n");
-
+	//CheckLicense();
+	
 	NIEngine::GetInstance()->Start();
 	
 	SocketServer server;
