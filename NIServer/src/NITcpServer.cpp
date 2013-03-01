901d8b4227277391a9b6b647b98ae2d456ab4209
len.zhong <lhongzhong@gmail.com>
2013-03-01 21:49:23 +0800

    fixed memory leak from QTcpServer.

--- NIServer/src/NITcpServer.cpp
+++ NIServer/src/NITcpServer.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 NITcpServer::NITcpServer(QObject *parent): QObject(parent)
 {
+	client = NULL;
+
     connect(&server,SIGNAL(newConnection()),this,SLOT(acceptConnection()));
 
     // Once command from client is received, forward it to the command parser.
@@ -33,8 +35,11 @@ NITcpServer::~NITcpServer()
 void NITcpServer::acceptConnection()
 {
     client = server.nextPendingConnection();
-    connect(client,SIGNAL(readyRead()),this,SLOT(startRead()));
-    connect(client,SIGNAL(disconnected()),this,SLOT(clientDisconnected()));
+	if (client != NULL)
+	{
+		connect(client,SIGNAL(readyRead()),this,SLOT(startRead()));
+		connect(client,SIGNAL(disconnected()),this,SLOT(clientDisconnected()));
+	}
 
     // Report to webserver that a new client connected.
     // Currently comment out as it takes too much time.
@@ -45,15 +50,18 @@ void NITcpServer::acceptConnection()
 void NITcpServer::startRead()
 {
     char buffer[BUFFER_SIZE] = {0};
-    client->read(buffer,client->bytesAvailable());
-    QString str(buffer);
+	if (client != NULL)
+	{
+		client->read(buffer,client->bytesAvailable());
+		QString str(buffer);
 
-    emit queryAvaiable(str);
+		emit queryAvaiable(str);
+	}
 }
 
 void NITcpServer::startWrite(QString echo)
 {
-    if(client)
+    if(client != NULL)
         client->write(echo.toAscii(),echo.length());
 
     //qDebug("%d %s",echo.length(),echo.toAscii());
@@ -61,6 +69,12 @@ void NITcpServer::startWrite(QString echo)
 
 void NITcpServer::clientDisconnected()
 {
+	if (client != NULL)
+	{
+		// Don't use "delete client;" here, as it'll crash, let QTcpSocket to handle it.
+		client->deleteLater(); 
+	}
+	
     // Report to webserver that the client is disconnected.
     //HttpRequest(clientOut.c_str());
 }
